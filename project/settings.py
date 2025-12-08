03f8b901763865517773f6f1feeb733f39328ff9
E-zn131 <eyadzanaty131@gmail.com>
2025-12-08 22:02:17 +0200

    Updating files

--- project/settings.py
+++ project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG= False
+DEBUG= True
 
 ALLOWED_HOSTS = [
     '*'
