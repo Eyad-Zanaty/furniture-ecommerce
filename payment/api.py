f3684e21f59decf2a1c0114ba33ea504fbac1297
E-zn131 <eyadzanaty131@gmail.com>
2025-12-09 20:19:51 +0200

    Updating files

--- payment/api.py
+++ payment/api.py
@@ -56,9 +56,7 @@ def payment_callback(request):
     
     data = request.data if request.method == 'POST' else request.GET
     
-    merchant_order_id= data.get('merchant_order_id')
-    order_id = merchant_order_id.split("-")[0]
-    order_id = int(order_id) 
+    order_id = data.get("order")
     
     try:
         get_order = order.objects.get(id=order_id)
