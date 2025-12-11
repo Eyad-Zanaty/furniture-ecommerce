635f8dee7e16e29624ca2917bfc60eb4f4c2ea42
E-zn131 <eyadzanaty131@gmail.com>
2025-12-11 17:03:04 +0200

    Fixing Caches with different user login

--- products/signals.py
+++ products/signals.py
@@ -2,7 +2,12 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.core.cache import cache
 from products.models import Product
+from users.models import CustomUser as User
 
 @receiver([post_save, post_delete], sender=Product)
 def clear_product_cache(sender, **kwargs):
+    cache.delete_pattern('*shop_page*')
+    
+@receiver([post_save, post_delete], sender=User)
+def clear_user_cache(sender, **kwargs):
     cache.delete_pattern('*shop_page*')
