5c470dfbb06c5742073fe252227b96c30205982a
E-zn131 <eyadzanaty131@gmail.com>
2025-12-12 19:18:11 +0200

    Shop product queryset

--- products/signals.py
+++ products/signals.py
@@ -1,4 +1,5 @@
 from django.db.models.signals import post_save, post_delete
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 from django.core.cache import cache
 from products.models import Product
@@ -9,5 +10,9 @@ def clear_product_cache(sender, **kwargs):
     cache.delete_pattern('*shop_page*')
     
 @receiver([post_save, post_delete], sender=User)
+def clear_user_cache(sender, **kwargs):
+    cache.delete_pattern('*shop_page*')
+
+@receiver([post_save, post_delete], user_logged_out)
 def clear_user_cache(sender, **kwargs):
     cache.delete_pattern('*shop_page*')
